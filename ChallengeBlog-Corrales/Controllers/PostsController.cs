f2e2d29a3758bc26de53b6588a66a7a612bdc0f1
Sergio <sergio_corrales27@hotmail.com>
2021-05-20 20:59:27 -0300

    Abm finalizado, con delete fisico

--- ChallengeBlog-Corrales/Controllers/PostsController.cs
+++ ChallengeBlog-Corrales/Controllers/PostsController.cs
@@ -19,10 +19,29 @@ namespace ChallengeBlog_Corrales.Controllers
             _Context = Context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int Id)
         {
             IEnumerable<Post> ListPosts = _Context.Post;
-            return View(ListPosts);
+            List<Post> LPost = new List<Post>();
+
+            if (ListPosts.Count() > 0)
+            {
+                Post post = new Post();
+                post = ListPosts.FirstOrDefault(x => x.Id == Id);
+
+                if (post.Picture != null)
+                {
+                    ViewBag.Base64String = "data:image/png;base64," + Convert.ToBase64String(post.Picture, 0, post.Picture.Length);
+                }
+
+                LPost.Add(post);
+            }
+
+
+            return View(LPost);
+
+            //IEnumerable<Post> ListPosts = _Context.Post;
+            //return View(ListPosts);
         }
 
         public IActionResult Create()
@@ -38,7 +57,8 @@ namespace ChallengeBlog_Corrales.Controllers
                 //Seteo el dia de hoy en la creacion del post
                 post.Creation_Date = DateTime.Today;
 
-                foreach (var file in Request.Form.Files) 
+                //busco la imagen y la convierto en byte[]
+                foreach (var file in Request.Form.Files)
                 {
                     MemoryStream ms = new MemoryStream();
                     file.CopyTo(ms);
@@ -47,12 +67,70 @@ namespace ChallengeBlog_Corrales.Controllers
                     ms.Dispose();
                 }
 
- 
+
                 _Context.Post.Add(post);
                 _Context.SaveChanges();
             }
-            
+
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Busco el post
+            var post = _Context.Post.Find(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                //busco la imagen y la convierto en byte[]
+                foreach (var file in Request.Form.Files)
+                {
+                    MemoryStream ms = new MemoryStream();
+                    file.CopyTo(ms);
+                    post.Picture = ms.ToArray();
+                    ms.Close();
+                    ms.Dispose();
+                }
+                _Context.Post.Update(post);
+                _Context.SaveChanges();
+
+                return RedirectToAction("Index", new { Id=post.Id });
+            }
+
+            return View();
+        }
+
+        //[HttpPost]
+        public IActionResult Delete(int? Id)
+        {
+            var post = _Context.Post.Find(Id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+                _Context.Post.Remove(post);
+                _Context.SaveChanges();
+
+                return RedirectToAction("Index", "Home", new { Id = post.Id });
+            
         }
     }
 }
